6ad61958f77420fe0bbeba583412367a76c47863
Aditya raj <aytida.dev@gmail.com>
2022-06-14 03:09:53 +0530

    mvc

--- script.js
+++ script.js
@@ -1,101 +1,56 @@
+//model
 let images = [];
 let input = [];
-let imagesRev = [];
 let i = 2;
 let j = 2;
-let a =0;
-let s=0;
-let currImage;
+let a = 0;
+let s = 0;
 
-function getImage() {
-    const userInput = document.getElementById('input').value;
+function push(userInput) {
     input.push(userInput);
+
+}
+
+function saveImage(response) { 
+    images.push(response);
+ }
+
+ function iAnds(){
+    i=i+1;
+    s=s+1;
+ }
+
+ function jaAnds(){
+    j = j + 1;
+    a = j - 2;
+    s = s - 1;
+ }
+
+//view
+function render() {
     const display = document.getElementById('display');
     display.innerText = "";
-    src = "https://source.unsplash.com/random/1024x500/?" + userInput + "=1"
-    
     fetch(src).then((response) => {
         const img = document.createElement('img');
         img.setAttribute('src', response.url);
         display.appendChild(img);
-        images.push(response);
-        // a=images.length;
+        saveImage(response);
     });
-    
-
-    
-    // fetch(src).then((response) => {currImage = response});
-    
-    
-    
-    document.getElementById('input').value = ''
 }
 
-function forward() {
-    const userInput = input[input.length - 1];
-    const display = document.getElementById('display');
-    display.innerText = "";
-    // const img = document.createElement('img');
-    src = "https://source.unsplash.com/random/1024x500/?" + userInput + "=" + i;
-    i = i + 1;
-    
-   
-
-
-    if (a===0) {
-        fetch(src).then((response) => {
-            const img = document.createElement('img');
-            img.setAttribute('src', response.url);
-            display.appendChild(img);
-            images.push(response);
-            j=2;
-            
-        });
-    }
-
-    else{
-        
-    
-        const img = document.createElement('img');
-        img.setAttribute('src', (images[images.length - a]).url);
-        a=a-1;
-        display.appendChild(img);
-
-    }
-    s=s+1;
-}
-
-function back() {
+function render1(x) {
     const display = document.getElementById('display');
     display.innerText = "";
     const img = document.createElement('img');
-    // imagesRev = images.slice().reverse();
-    // src = imagesRev[j];
-    // j = j + 1;
-    img.setAttribute('src', (images[images.length - j]).url);
-    j=j+1;
+    img.setAttribute('src', (images[images.length - x]).url);
     display.appendChild(img);
-    a = j - 2;
-    s=s-1;
-    // fetch(src).then((response) => {currImage = response});
 }
-
-// function download() {
-//     const a = document.createElement('a')
-//     a.href=images[s].url;
-//     // a.href="i.jpg"
-//     a.download = "jii"
-//     document.body.appendChild(a)
-//     a.setAttribute('target','none');
-//     a.click()
-//     document.body.removeChild(a)
-//   }
-  
-function download(){
+//control
+function download() {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", images[s].url, true);
     xhr.responseType = "blob";
-    xhr.onload = function(){
+    xhr.onload = function () {
         var urlCreator = window.URL || window.webkitURL;
         var imageUrl = urlCreator.createObjectURL(this.response);
         var tag = document.createElement('a');
@@ -106,4 +61,41 @@ function download(){
         document.body.removeChild(tag);
     }
     xhr.send();
+}
+
+function getImage() {
+    const userInput = document.getElementById('input').value;
+    push(userInput);
+    src = "https://source.unsplash.com/random/1024x500/?" + userInput + "=1"
+
+    render();
+
+    document.getElementById('input').value = ''
+}
+
+function forward() {
+    const userInput = input[input.length - 1];
+
+    src = "https://source.unsplash.com/random/1024x500/?" + userInput + "=" + i;
+    
+    iAnds();
+
+    if (a === 0) {
+        render();
+        j = 2;
+    }
+
+    else {
+
+        render1(a);
+
+        a = a - 1;
+
+    }
+    
+}
+
+function back() {
+    render1(j);
+   jaAnds();
 }
